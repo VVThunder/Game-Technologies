6d3bb65d45a82facd2c3a9daf90f038ae1ab03b5
VVThunder <145505541+VVThunder@users.noreply.github.com>
2023-09-25 19:36:13 +0100

    create with code - mission + bonus

--- Projects/Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Projects/Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 5.0f;
+    public string inputID;
+
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
+    private float speed = 20.0f;
     private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
@@ -19,12 +25,22 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // This is where we get player input
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
 
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
         // We turn the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        // We Switch the camera angle
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+
+
     }
 }
